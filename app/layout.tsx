e2dd4fc4d16ff00978e2bbb20bd88b4200f2c6de
npcTung <tungtrinhthanh0311@gmail.com>
2025-07-27 16:37:11 +0700

    add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script"; // Thêm dòng này
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MCDF9GSM');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+              <noscript>
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MCDF9GSM"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              </noscript>
+            `,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         <GalaxyBackground />
         <Header />
         {children}
